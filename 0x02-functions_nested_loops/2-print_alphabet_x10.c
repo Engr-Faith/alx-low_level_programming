ad85b23d0057cfd118aef334279b869ead5744d1
Faith <engr.eyitope@gmail.com>
2022-09-14 15:09:20 -0700

    wlcome

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-    int i, j;
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 'a'; j <= 'z'; j++)
-        {
-            _putchar(j);
-          }
-         _putchar('\n');
-    }
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n')
+}
 }
