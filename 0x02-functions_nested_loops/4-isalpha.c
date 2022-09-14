9886a59e38ec0646734408ad817e02d48914a5d9
Faith <engr.eyitope@gmail.com>
2022-09-14 15:40:34 -0700

    welcome

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - it checks for alphabets
+ * @c - is a parmeter
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c > 'A' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
