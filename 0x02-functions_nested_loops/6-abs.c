5751c9f580bd47a52a69679cac58910757edbfb9
Faith <engr.eyitope@gmail.com>
2022-09-14 16:23:21 -0700

    welcome

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - gives the absolute value
+ * @c : a parameter
+ * Return: always 0.
+ */
+
+int _abs(int c)
+{
+	int test;
+	if (c < 0)
+	{
+		test = c * -1;
+	}
+	else
+	{
+		test = c * 1;
+	}
+	return (test);
+}
