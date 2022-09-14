7ee8545b92800e623dedbf1296b8065e7944c79d
Faith <engr.eyitope@gmail.com>
2022-09-14 15:25:28 -0700

    welcome

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * islower - checks for lower case
+ * retunr: always 0
+ */
+inst _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+
+	{
+		return(0);
+	}
+}
