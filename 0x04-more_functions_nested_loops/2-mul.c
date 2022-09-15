d8353c1788af98f5486d68fde2c7c3584cf8696e
Faith <engr.eyitope@gmail.com>
2022-09-15 09:07:30 -0700

    welcoem

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two digits
+ * @a : first param;
+ * @b : second param;
+ * Return : always 0
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
