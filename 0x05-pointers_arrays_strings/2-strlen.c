0265c0592747215eaa9721240446eb399bf3f2f6
Faith <engr.eyitope@gmail.com>
2022-09-20 13:01:30 -0700

    welcome

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a param;
+ * return: returns a value
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	len = strlen(s);
+	return (len);
+}
